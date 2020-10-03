fff627f3f672ab72c9ff8606f70de65b395f5e07
alex <alex@levelism.com>
2020-10-03 11:39:20 -0700

    added assert to make sure ring is a circle.

--- Assets/Scripts/CatRingBehavior.cs
+++ Assets/Scripts/CatRingBehavior.cs
@@ -15,6 +15,11 @@ public class CatRingBehavior : MonoBehaviour
         parentCylinder = gameObject.GetComponentInParent<CapsuleCollider>();
 
         var radius = parentCylinder.radius;
+
+        // want to make sure we have a circle for our ring and not an ellipses. This is in case I accidentally
+        // change the scale of the X/Z axis non-uniformly
+        Debug.Assert(Mathf.Approximately(gameObject.transform.localScale.x,gameObject.transform.localScale.z));
+
         Vector3 spawnPosition;
         spawnPosition.y = .5f;
         spawnPosition.x = radius * gameObject.transform.localScale.x;
