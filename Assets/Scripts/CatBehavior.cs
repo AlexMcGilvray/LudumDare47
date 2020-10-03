fd8a4c133fbce509c95eee7b72585fa9d4ccd7ff
alex <alex@levelism.com>
2020-10-03 11:36:46 -0700

    basic spawning of cat around ring

--- Assets/Scripts/CatBehavior.cs
+++ Assets/Scripts/CatBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CatBehavior : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/CatRingBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatRingBehavior : MonoBehaviour
+{
+
+    private CapsuleCollider parentCylinder;
+
+    public GameObject catTemplateObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentCylinder = gameObject.GetComponentInParent<CapsuleCollider>();
+
+        var radius = parentCylinder.radius;
+        Vector3 spawnPosition;
+        spawnPosition.y = .5f;
+        spawnPosition.x = radius * gameObject.transform.localScale.x;
+        spawnPosition.z = 0;
+
+        Instantiate(catTemplateObject,spawnPosition,Quaternion.identity);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
