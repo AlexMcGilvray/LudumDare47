aca93e46be1e85a4da1ff2f4d3ae84d1ead1bc81
alex <alex@levelism.com>
2020-10-03 18:03:33 -0700

    camera now follows player (TODO make it catch up to player rather than copy the position).

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = gameObject.transform.position;
+        pos.x = player.transform.position.x;
+        pos.z = player.transform.position.z;
+        gameObject.transform.position = pos;
+    }
+
+    private Camera _camera;
+}
