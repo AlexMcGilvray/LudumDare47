fcc6c06f4f4de6286f56bca852a971e1b6c28122
alex <alex@levelism.com>
2020-10-04 05:48:29 -0700

    added leaving state to the cat, refactored a bit of code.

--- Assets/Scripts/CatBehavior.cs
+++ Assets/Scripts/CatBehavior.cs
@@ -13,13 +13,21 @@ public class CatBehavior : MonoBehaviour
 {
     public float ricochetSpeed = 3.0f;
 
+    public float ricochetSpeedIncreaseOnBounce = 1.0f;
+
     public float richochetCollisionResponseDelaySeconds = 2.0f;
+
+    public int bouncesBeforeLeaving = 5;
+
+    public int bounceRandomRange = 3;
 
     public void ChangeToRicochetMode()
     {
         SetState(CatState.Ricochet);
         _ricochetDirection = -gameObject.transform.right;
         _richochetCollisionResponseDelaySeconds = richochetCollisionResponseDelaySeconds;
+
+        _bouncesLeft = bouncesBeforeLeaving + (int)(Random.value * bounceRandomRange);
     }
     public void SetState(CatState state)
     {
@@ -27,22 +35,28 @@ public class CatBehavior : MonoBehaviour
     }
     void Start()
     {
-
+        _ricochetSpeed = ricochetSpeed;
     }
 
     void Update()
     {
+        void updateTransform()
+        {
+            gameObject.transform.position += _ricochetDirection * _ricochetSpeed * Time.deltaTime;
+            float directionAngle = Mathf.Atan2(-_ricochetDirection.x, -_ricochetDirection.z);
+            gameObject.transform.rotation = Quaternion.Euler(0, directionAngle * Mathf.Rad2Deg, 0);
+        }
+
         switch (_state)
         {
             case CatState.Ring:
                 break;
             case CatState.Ricochet:
-                gameObject.transform.position += _ricochetDirection * ricochetSpeed * Time.deltaTime;
                 _richochetCollisionResponseDelaySeconds -= Time.deltaTime;
-                float directionAngle = Mathf.Atan2(-_ricochetDirection.x, -_ricochetDirection.z);
-                gameObject.transform.rotation = Quaternion.Euler(0,directionAngle * Mathf.Rad2Deg,0);
+                updateTransform();
                 break;
             case CatState.Leaving:
+                updateTransform();
                 break;
         }
     }
@@ -57,10 +71,16 @@ public class CatBehavior : MonoBehaviour
                 _ricochetDirection = -_ricochetDirection;
                 _ricochetDirection.x += Random.value / 2.0f;
                 _ricochetDirection.z += Random.value / 2.0f;
+                _ricochetSpeed += ricochetSpeedIncreaseOnBounce;
+                _bouncesLeft--;
+                if (_bouncesLeft <= 0)
+                {
+                    _ricochetSpeed = _ricochetSpeed * 2.0f;
+                    SetState(CatState.Leaving);
+                }
                 //Debug.Log("Cat collided with " + other.gameObject.name + " in OnTriggerEnter");
             }
         }
-
     }
 
     // void OnCollisionEnter(Collision collision)
@@ -73,6 +93,11 @@ public class CatBehavior : MonoBehaviour
     //     }
 
     // }
+
+    // when this hits zero the cat begins to leave the area
+
+    private float _ricochetSpeed;
+    private int _bouncesLeft;
 
     private CatState _state = CatState.Ring;
 
