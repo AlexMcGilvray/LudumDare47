52a45ede59cbef18c8280b8442d27b669911efd7
alex <alex@levelism.com>
2020-10-04 06:29:53 -0700

    basic score system and UI to display score. Score increaes gradually.

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class GameManagerBehavior : MonoBehaviour
 {
-    //public GameObject player;
+    public GameObject uiGameObject;
+
+    public float pointsPerSecond = 10;
 
     //public GameObject catRing;
 
     // Start is called before the first frame update
     void Start()
     {
+        _ui = uiGameObject.GetComponent<UIBehavior>();
        // _catRing = catRing.GetComponent<CatRingBehavior>();
     }
+
+    void AddToScore(int points)
+    {
+        _score += points;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        _ui.UpdateScore((int)_score);
+    }
+
+    private float _score = 0;
+
+    private UIBehavior _ui;
 
     // Update is called once per frame
     void Update()
     {     
+        _score += pointsPerSecond * Time.deltaTime;
+        UpdateUI();
         //player.GetComponentInChildren<MeshCollider>().o
         //Physics.OverlapBox()
         
